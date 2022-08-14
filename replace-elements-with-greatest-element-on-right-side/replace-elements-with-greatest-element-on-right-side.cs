3874b9ecec377f76e056a487f8bf0c9208494006
Jodex Saint-Vil <jsaintvi@aol.com>
2022-08-14 19:42:51 -0400

    Time: 169 ms, Memory: 50.4 MB - LeetHub

--- replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cs
+++ replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cs
@@ -2,24 +2,23 @@ public class Solution {
     public int[] ReplaceElements(int[] arr) {
         int n = arr.Length;
         
-        if(n == 1) {
-            arr[0] = -1;
+        if(n==1){
+            arr[n-1] = -1;
             
             return arr;
-        } 
-        
-        for(int i = 0; i < n; i++ ) {
-            int maxSoFar = -1;
-            for(int j = i+1; j < n; j++) {
-                if(arr[j] > maxSoFar) {
-                    maxSoFar = arr[j];
-                }
-            }
-            
-            arr[i] = maxSoFar;
         }
         
-        arr[n-1] =-1;
+        for(int i= arr.Length-1; i > 0; i--)
+        {
+            if (arr[i] > arr[i-1]) 
+                arr[i-1] = arr[i];
+        }
+        
+        for(int i=1; i<arr.Length; i++)
+        {
+            arr[i-1] = arr[i];
+        }
+        arr[arr.Length -1] = -1;
         return arr;
     }
 }
