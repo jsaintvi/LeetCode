97eb4bbea98cad121a42eb041d322c87a7d9a4c0
Jodex Saint-Vil <jsaintvi@aol.com>
2022-08-14 19:54:37 -0400

    Time: 215 ms, Memory: 47.1 MB - LeetHub

--- replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cs
+++ replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cs
@@ -1,24 +1,21 @@
 public class Solution {
     public int[] ReplaceElements(int[] arr) {
+        
         int n = arr.Length;
-        
-        if(n==1){
-            arr[n-1] = -1;
+        int currentMax = arr[n - 1];
+        for (var i = arr.Length - 1; i >= 0; i--) {
             
-            return arr;
+            var temp = arr[i];
+            if (arr[i] > currentMax) {
+                arr[i] = currentMax;
+                currentMax = temp;
+            } else {
+                arr[i] = currentMax;
+            }
+            
         }
         
-        for(int i= arr.Length-1; i > 0; i--)
-        {
-            if (arr[i] > arr[i-1]) 
-                arr[i-1] = arr[i];
-        }
-        
-        for(int i=1; i<arr.Length; i++)
-        {
-            arr[i-1] = arr[i];
-        }
-        arr[arr.Length -1] = -1;
+        arr[n-1] = -1;
         return arr;
     }
 }
