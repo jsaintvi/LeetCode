b6d7ef76e37849a04605e75d1e2c4026e307dee8
Jodex Saint-Vil <jsaintvi@aol.com>
2022-08-14 20:45:50 -0400

    Time: 157 ms, Memory: 41.5 MB - LeetHub

---
+++ remove-element/remove-element.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int len = nums.Length;
+        
+        int i = 0;
+        while(i < len) {
+            if(nums[i] == val) {
+                nums[i] = nums[len-1];
+                len--;
+            }else{
+                i++;
+            }
+        }
+        
+        return len;
+    }
+
+}
