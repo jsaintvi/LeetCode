1a340215f1259c177c3aa8fcbfe04e6b71ffd7b5
Jodex Saint-Vil <jsaintvi@aol.com>
2022-08-14 20:34:15 -0400

    Time: 242 ms, Memory: 44.7 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cs
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cs
@@ -7,8 +7,7 @@ public class Solution {
         for (int j = 1; j < nums.Length; j++) {
             if(nums[j] == nums[j-1]) continue; // skip over dupes
 
-            nums[insertIndex] = nums[j];
-            insertIndex++;
+            nums[insertIndex++] = nums[j];
         }
         
         return insertIndex;
