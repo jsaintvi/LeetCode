87633ebc53c403d8c99ec04342c446c12a8a9823
Jodex Saint-Vil <jsaintvi@aol.com>
2022-08-31 20:40:25 -0400

    Time: 121 ms (55.88%), Space: 37.6 MB (43.14%) - LeetHub

---
+++ 250-count-univalue-subtrees/250-count-univalue-subtrees.cs
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int CountUnivalSubtrees(TreeNode root) {
+        if(root == null) return 0;
+        int count = 0;
+        IsUni(root, ref count);
+        
+        return count;
+    }
+    
+    private bool IsUni(TreeNode node, ref int count) {
+        /*
+            2 criteria for univalue subtree"
+            1- Node has no children
+            2- All of th node children are univalues subtrees and the node and its children all have the same value
+        
+        */
+        if(node.left == null && node.right == null) { // leaf nodes
+            count++;
+            return true;
+        }
+        
+        bool is_uniVal = true;
+        
+        if(node.left != null)
+            is_uniVal = IsUni(node.left, ref count) && is_uniVal && node.left.val == node.val;
+        
+        if(node.right != null)
+            is_uniVal = IsUni(node.right, ref count) && is_uniVal && node.right.val == node.val;
+        
+        
+        if(!is_uniVal)
+            return false;
+        
+        count++;
+        return true;
+    }
+}
