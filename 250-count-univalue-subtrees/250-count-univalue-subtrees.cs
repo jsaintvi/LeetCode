0ab7f935edbf5bc4be26a02794477b829ffe7fea
Jodex Saint-Vil <jsaintvi@aol.com>
2022-08-31 20:44:06 -0400

    Time: 174 ms (6.86%), Space: 37.6 MB (43.14%) - LeetHub

--- 250-count-univalue-subtrees/250-count-univalue-subtrees.cs
+++ 250-count-univalue-subtrees/250-count-univalue-subtrees.cs
@@ -20,31 +20,52 @@ public class Solution {
         return count;
     }
     
-    private bool IsUni(TreeNode node, ref int count) {
+//     private bool IsUni(TreeNode node, ref int count) {
+//         /*
+//             2 criteria for univalue subtree"
+//             1- Node has no children
+//             2- All of th node children are univalues subtrees and the node and its children all have the same value
+        
+//         */
+//         if(node.left == null && node.right == null) { // leaf nodes
+//             count++;
+//             return true;
+//         }
+        
+//         bool is_uniVal = true;
+        
+//         if(node.left != null)
+//             is_uniVal = IsUni(node.left, ref count) && is_uniVal && node.left.val == node.val;
+        
+//         if(node.right != null)
+//             is_uniVal = IsUni(node.right, ref count) && is_uniVal && node.right.val == node.val;
+        
+        
+//         if(!is_uniVal)
+//             return false;
+        
+//         count++;
+//         return true;
+//     }
+    
+    public bool IsUni(TreeNode root, ref int count) {
+        
         /*
-            2 criteria for univalue subtree"
+            TWO criteria for univalue subtree"
             1- Node has no children
-            2- All of th node children are univalues subtrees and the node and its children all have the same value
+            2- All of th node children are univalues subtrees and the node and its children all have the                same value
         
         */
-        if(node.left == null && node.right == null) { // leaf nodes
+        
+        if (root is null) return true;
+        bool left = IsUni(root.left, ref count);
+        bool right = IsUni(root.right, ref count);
+        if (left && right && 
+           (root.left == null || root.val == root.left.val) && 
+           (root.right == null || root.val == root.right.val)) {
             count++;
             return true;
         }
-        
-        bool is_uniVal = true;
-        
-        if(node.left != null)
-            is_uniVal = IsUni(node.left, ref count) && is_uniVal && node.left.val == node.val;
-        
-        if(node.right != null)
-            is_uniVal = IsUni(node.right, ref count) && is_uniVal && node.right.val == node.val;
-        
-        
-        if(!is_uniVal)
-            return false;
-        
-        count++;
-        return true;
+        return false;
     }
 }
