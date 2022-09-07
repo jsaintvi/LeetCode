5ff1da3bc391c00900cdcaf563335bef63ceab97
Jodex Saint-Vil <jsaintvi@aol.com>
2022-09-06 21:36:40 -0400

    Time: 204 ms, Memory: 41.3 MB - LeetHub

---
+++ binary-search/binary-search.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int len = nums.Length;
+        
+        int low = 0;
+        int high = len-1;
+        
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            } else if(target > nums[mid]) {
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        
+        return -1;
+    }
+}
