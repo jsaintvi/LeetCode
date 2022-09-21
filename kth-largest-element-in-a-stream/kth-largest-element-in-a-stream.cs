905a76163d08263134b71dcb8e810625fa65ab29
Jodex Saint-Vil <jsaintvi@aol.com>
2022-09-20 22:25:34 -0400

    Time: 324 ms, Memory: 49.7 MB - LeetHub

---
+++ kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cs
@@ -0,0 +1,25 @@
+public class KthLargest {
+    private readonly PriorityQueue<int,int> pq = new ();
+    private readonly int pqSize;
+    public KthLargest(int k, int[] nums) {
+        this.pqSize = k;
+        pq = new(pqSize);
+        
+        foreach(var num in nums)
+            Add(num);
+    }
+    
+    public int Add(int val) {
+        pq.Enqueue(val, val);
+        
+        if(pq.Count > pqSize) pq.Dequeue();
+        
+        return pq.Peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.Add(val);
+ */
