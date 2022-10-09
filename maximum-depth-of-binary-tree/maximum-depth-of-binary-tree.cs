4ba7e4f7d97eee1ff0a3746af5d2a22ea6c2e32c
Jodex Saint-Vil <jsaintvi@aol.com>
2022-10-09 12:28:16 -0400

    Time: 139 ms, Memory: 38.2 MB - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cs
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cs
@@ -12,11 +12,42 @@
  * }
  */
 public class Solution {
+private class Pair{
+    public TreeNode Node;
+    public int Depth;
+    
+    public Pair(TreeNode node, int depth) {
+        this.Node = node;
+        this.Depth = depth;
+    }
+}
  private int count = 0;
     public int MaxDepth(TreeNode root) {
         if(root == null) return 0;
         
-        return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+        Stack<Pair> st = new();
+        st.Push(new Pair(root, 1));
+        
+        int max_depth = 0;
+        
+        while(st.Count > 0) {
+            var travPair = st.Pop();
+            
+            var travDepth = travPair.Depth;
+            var travNode = travPair.Node;
+            
+            max_depth = Math.Max(max_depth, travDepth);
+            
+            if(travNode.left != null) {
+                st.Push(new Pair(travNode.left, travDepth+1));
+            }
+            
+            if(travNode.right != null) {
+                st.Push(new Pair(travNode.right, travDepth+1));
+            }
+        }
+        
+        return max_depth;
     }
     
 
