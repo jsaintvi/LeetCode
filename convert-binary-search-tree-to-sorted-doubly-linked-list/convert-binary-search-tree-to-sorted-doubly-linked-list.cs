3715af0f8fdeaf1f25a12c6e9deafe5df275c352
Jodex Saint-Vil <jsaintvi@aol.com>
2022-10-13 22:18:06 -0400

    Time: 97 ms, Memory: 39.2 MB - LeetHub

--- convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.cs
+++ convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.cs
@@ -23,39 +23,43 @@ public class Node {
 
 public class Solution {
             
-    Node first = null;
-    Node last = null;
     public Node TreeToDoublyList(Node root) {
-        if(root == null) return null;
-
+        if (root == null) {
+            return null;
+        }
         
-        DFS(root);
+        Node head = null;
+        Stack<Node> stack = new Stack<Node>();
+        Node cur = root;
+        Node prev = null;
         
-        last.right = first;
-        first.left = last;
+        // in order traversal
+        while (stack.Count != 0 || cur != null) {
+            // dig as far left
+            while (cur != null) {
+                stack.Push(cur);
+                cur = cur.left;
+            }
+            
+            cur = stack.Pop();
+            head = head ?? cur; // assign left most node to head
+            
+            if (prev != null) {
+                // link the 2 nodes
+                prev.right = cur;
+                cur.left = prev;
+            }
+            
+            prev = cur;
+            cur = cur.right;
+        }
         
-        return first;
+		// link last element with head. If only one Node, still linking to itself.
+        head.left = prev;
+        prev.right = head;
+        
+        return head;
         
     }
     
-    private void DFS(Node root) {
-        if(root == null) return;
-        
-        DFS(root.left);
-        
-        if(last != null) {
-            // link prev node (last) with curr node (root)
-            last.right = root;
-            
-            root.left = last;
-        }else {
-            // keep the smallest node 
-            first = root;
-        }
-        
-        last = root;
-        
-        // right
-        DFS(root.right);
-    }
 }
