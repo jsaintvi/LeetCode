f043fa0d9db0dbc91b6ee41d495c7c24fa54801f
Jodex Saint-Vil <jsaintvi@aol.com>
2022-10-19 22:44:56 -0400

    Time: 145 ms, Memory: 36.5 MB - LeetHub

--- house-robber/house-robber.cs
+++ house-robber/house-robber.cs
@@ -1,18 +1,24 @@
 public class Solution {
     public int Rob(int[] nums) {
-        int n = nums.Length;
-        if(n == 1){
+        int n = nums.Length;        
+
+        Dictionary<int,int> cache = new();
+        
+        return Dp(nums, n-1, cache);
+        
+    }
+    
+    private int Dp(int[] nums, int index, Dictionary<int,int> cache) {
+        if(index == 0)
             return nums[0];
-        }
+        if(index ==1)
+            return Math.Max(nums[0], nums[1]);
         
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.Max(nums[0], nums[1]);
+        if(cache.ContainsKey(index))
+            return cache[index];
         
-        for(int i = 2; i < n; i++) {
-            dp[i] = Math.Max(dp[i-1], dp[i-2] + nums[i]);
-        }
+        cache.Add(index, Math.Max( Dp(nums, index-1, cache),  Dp(nums, index-2, cache) + nums[index]));
         
-        return dp[n-1];
+        return cache[index];
     }
 }
