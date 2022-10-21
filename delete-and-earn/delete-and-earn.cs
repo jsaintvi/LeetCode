691d590d42794bc10d8a2f6944621f2cf7ecddda
Jodex Saint-Vil <jsaintvi@aol.com>
2022-10-20 22:49:38 -0400

    Time: 168 ms, Memory: 39.2 MB - LeetHub

--- delete-and-earn/delete-and-earn.cs
+++ delete-and-earn/delete-and-earn.cs
@@ -1,9 +1,21 @@
 public class Solution {
     public int DeleteAndEarn(int[] nums) {
         Dictionary<int,int> points = ComputePoints(nums);
-        int maxNum = MaxNum(nums);
+        int maxNum = FindMax(nums);
         
-        return MaxPoints(nums, maxNum, points, new Dictionary<int,int>());
+        int[] maxPoints = new int[maxNum + 1];
+        maxPoints[0] = 0;
+        maxPoints[1] =  points.ContainsKey(1) ? points[1] : 0;
+        
+        for(int num = 2; num < maxPoints.Length; num++) {
+            int gain = points.ContainsKey(num) ? points[num] : 0;
+            int notTakeCurrNum = maxPoints[num-1];
+            int takeCurrNum = maxPoints[num-2] + gain;
+            
+            maxPoints[num] = Math.Max(notTakeCurrNum, takeCurrNum);
+        }
+        
+        return maxPoints[maxPoints.Length -1];
     }
     
     private Dictionary<int,int> ComputePoints(int[]nums){
@@ -18,31 +30,12 @@ public class Solution {
         return points;
     }
     
-    private int MaxNum(int[]nums){
+    private int FindMax(int[]nums){
         int maxNum = 0;
         foreach(int num in nums){
             maxNum = Math.Max(maxNum, num);
         }
         
         return maxNum;
-    }
-    
-    private int MaxPoints(int[] nums, int num, Dictionary<int,int> points, Dictionary<int,int> cache){
-        if(num == 0) return 0;
-        if(num == 1) 
-            return points.ContainsKey(1) ? points[1] : 0;
-        
-        if(!cache.ContainsKey(num)){
-            int gain = points.ContainsKey(num) ? points[num] : 0;
-            
-            int notTakeCurrNum = MaxPoints(nums, num-1, points, cache);
-            int takeCurrNum = MaxPoints(nums, num-2, points, cache) + gain;
-            var maxPoint =  Math.Max(notTakeCurrNum, takeCurrNum);
-            cache.Add(num,maxPoint);
-        }
-            
-        
-        
-        return cache[num];
     }
 }
