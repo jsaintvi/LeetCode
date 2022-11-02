8bd8739ca14839c07e307836a1863829bd2d32b0
Jodex Saint-Vil <jsaintvi@aol.com>
2022-11-01 22:08:55 -0400

    Time: 141 ms, Memory: 37.7 MB - LeetHub

--- 0min-cost-climbing-stairs/0min-cost-climbing-stairs.cs
+++ 0min-cost-climbing-stairs/0min-cost-climbing-stairs.cs
@@ -1,18 +1,15 @@
 public class Solution {
     public int MinCostClimbingStairs(int[] cost) {
 
-        int[] dp = new int[cost.Length+1]; // +1 to treat the top floor as the step to reach.
-        dp[0] = 0;
-        dp[1] = 0;
+        int takeOneStep = 0;
+        int takeTwoStep = 0;
         
-        int n = dp.Length;
-        for(int i = 2; i < n; i++) {
-            int takeOneStep = cost[i-1] + dp[i-1];
-            int takeTwoStep = cost[i-2] + dp[i-2];
-            
-            dp[i] = Math.Min(takeOneStep, takeTwoStep);
+        for(int i = 2; i < cost.Length + 1; i++) { // + 1 as we treat the 'top' floor as the final step
+            int temp = takeOneStep;
+            takeOneStep = Math.Min(takeOneStep + cost[i-1], takeTwoStep + cost[i-2]);
+            takeTwoStep = temp;
         }
         
-        return dp[n-1];
+        return takeOneStep;
     }
 }
