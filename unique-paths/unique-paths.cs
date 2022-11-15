e115596d99e9369c184f7c7736947ebbf54badd8
Jodex Saint-Vil <jsaintvi@aol.com>
2022-11-14 23:11:20 -0500

    Time: 28 ms, Memory: 25.1 MB - LeetHub

---
+++ unique-paths/unique-paths.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int UniquePaths(int m, int n) {
+        int[,] dp = new int[m, n];
+        
+ 
+        for(int row = 0; row < m; row++)
+            for(int col = 0; col < n; col++)
+                dp[row,col] = 1;
+        
+        for(int row = 1; row < m; row++ )
+            for(int col = 1; col < n; col++)
+                dp[row, col] = dp[row-1, col] + dp[row, col - 1];
+        
+        return dp[m-1,n-1];
+    }
+}
