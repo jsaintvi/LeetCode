16ac97d9056f4b4f65156239bfe3336d843672c9
Jodex Saint-Vil <jsaintvi@aol.com>
2022-11-15 23:15:48 -0500

    Time: 141 ms, Memory: 37.5 MB - LeetHub

--- unique-paths-ii/unique-paths-ii.cs
+++ unique-paths-ii/unique-paths-ii.cs
@@ -1,37 +1,31 @@
 public class Solution {
     public int UniquePathsWithObstacles(int[][] obstacleGrid) {
+        var memo = new Dictionary<string, int>();
         
-        int rowCount = obstacleGrid.Length;
-        int colCount = obstacleGrid[0].Length;
+        return Solve(obstacleGrid, 0, 0, memo);
+    }
+    
+    private int Solve(int[][] grid, int row, int col, Dictionary<string, int> mem)
+    {
+        var key = $"{row}_{col}";
+        if (mem.ContainsKey(key))
+        {
+            return mem[key];
+        }
         
-        // if first cell has an obstacle, there is no path to destination
-        if(obstacleGrid[0][0] == 1){
+        // out of bounds or obstacles
+        if (row >= grid.Length || col >= grid[row].Length || grid[row][col] == 1)
+        {
             return 0;
         }
-        
-        // number of ways of reaching starting cell
-        obstacleGrid[0][0] = 1;
-        
-        // filling the values for the first column
-        for(int i = 1; i < rowCount; i++)
-            obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) ? 1 : 0;
-        
-        // filling the values for the first row
-        for(int i = 1; i < colCount; i++)
-            obstacleGrid[0][i] = (obstacleGrid[0][i] == 0 && obstacleGrid[0][i - 1] == 1) ? 1 : 0;
-        
 
-        for(int i = 1; i < rowCount; i++) {
-            for(int j = 1; j < colCount; j++) {
-                if(obstacleGrid[i][j] == 1) { //obstacle, do not include
-                    obstacleGrid[i][j] = 0;
-                }else{
-                    // number of ways of reaching this cell is : number of ways of reaching the cell to its left and number of ways of reaching the cell above it
-                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
-                }
-            }
+        if (row == grid.Length - 1 && col == grid[row].Length - 1)
+        {
+            return 1;
         }
         
-        return obstacleGrid[rowCount - 1][colCount - 1];
+        mem.Add(key, Solve(grid, row + 1, col, mem) + Solve(grid, row, col + 1, mem));
+        
+        return mem[key];
     }
 }
