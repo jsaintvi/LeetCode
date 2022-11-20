31cb6a74be74403c05514e688c42dd509159fdf9
Jodex Saint-Vil <jsaintvi@aol.com>
2022-11-19 19:55:13 -0500

    Time: 247 ms, Memory: 40 MB - LeetHub

--- minimum-path-sum/minimum-path-sum.cs
+++ minimum-path-sum/minimum-path-sum.cs
@@ -2,39 +2,42 @@ public class Solution {
     public int MinPathSum(int[][] grid) {
         int m = grid.Length, n = grid[0].Length;
         
-        //Console.WriteLine("POPULATING first column");
-        for(int i = 1; i < m; ++i) {
-            grid[i][0] += grid[i - 1][0];
-           // Print(grid, i , 0);
+        
+        var memo = new int[m,n];
+        Fill(memo, -1);
+        
+        return Helper(grid, m - 1, n - 1, memo);
+    }
+    
+
+    public int Helper(int[][] grid, int i, int j, int[,]memo) 
+    {
+        if(i < 0 || j < 0) return int.MaxValue;
+        
+        if(i== 0 && j == 0) { // first cell
+            memo[i,j] = grid[0][0];    
+            return memo[i,j];
         }
         
-        //Console.WriteLine("POPULATING first row");
-        for(int j = 1; j < n; ++j) {
-            grid[0][j] += grid[0][j - 1];
-        // Print(grid, 0, j);   
-        }
-
-        //Console.WriteLine("------------------------");
-        for(int i = 1; i < m; ++i){
-            for(int j = 1; j < n; ++j){
-                grid[i][j] += Math.Min(grid[i-1][j], grid[i][j-1]);
-                //Print(grid, 0, j);   
-
-            }
-        }
-        return grid[m-1][n-1];
+        if(memo[i,j] != -1) return memo[i,j];
+        
+        // deciding whether to go right or down (in reverse in our case)
+        int takeDown =  Helper(grid, i-1, j, memo);
+        int takeRight = Helper(grid, i, j-1, memo);
+        
+        
+        Console.WriteLine($"To get to [{i}][{j}], we can go down with cost of {takeDown} or right with cost of {takeRight} added to currValue of {grid[i][j]}");
+        memo[i,j] = grid[i][j] + Math.Min(takeDown, takeRight); 
+        
+        return memo[i,j];
     }
     
-    private void Print(int[][] grid) {
-        for(int i = 0; i < grid.Length; i++) {
-            for(int j = 0; j < grid[0].Length; j++) {
-                Console.WriteLine($"Value at index[{i}][{j}] is {grid[i][j]}");
-            }
-        }
-    }
-    
-    private void Print(int[][]grid, int i , int j){
-        Console.WriteLine($"Value at index[{i}][{j}] is {grid[i][j]}");
-
+    private void Fill(int[,] memo, int val){
+        
+        int m = memo.GetLength(0);
+        int n = memo.GetLength(1);
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++)
+                memo[i,j] = val;
     }
 }
