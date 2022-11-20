69000e76301fbbe358f986d7fca7ac661c57d305
Jodex Saint-Vil <jsaintvi@aol.com>
2022-11-19 19:28:43 -0500

    Time: 118 ms, Memory: 38.7 MB - LeetHub

--- minimum-path-sum/minimum-path-sum.cs
+++ minimum-path-sum/minimum-path-sum.cs
@@ -1,23 +1,40 @@
 public class Solution {
     public int MinPathSum(int[][] grid) {
-        int rows= grid.Length;
-        int cols = grid[0].Length;
-        int[,] dp = new int[rows, cols];
+        int m = grid.Length, n = grid[0].Length;
         
-        for(int row = rows-1; row >= 0; row--) {
-            for(int col = cols - 1; col >= 0; col--) {
-                if(row == rows - 1 && col != cols-1) {
-                    dp[row,col] = grid[row][col] + dp[row, col+1];
-                } else if(col == cols - 1 && row != rows-1){
-                    dp[row,col] = grid[row][col] + dp[row+1, col];
-                } else if(col != cols-1 && row != rows - 1){
-                    dp[row,col] = grid[row][col] + Math.Min(dp[row+1, col], dp[row, col+1]);
-                }else{
-                    dp[row,col] = grid[row][col];
-                }
-            }
+        //Console.WriteLine("POPULATING first column");
+        for(int i = 1; i < m; ++i) {
+            grid[i][0] += grid[i - 1][0];
+           // Print(grid, i , 0);
         }
         
-        return dp[0,0];
+        //Console.WriteLine("POPULATING first row");
+        for(int j = 1; j < n; ++j) {
+            grid[0][j] += grid[0][j - 1];
+        // Print(grid, 0, j);   
+        }
+
+        //Console.WriteLine("------------------------");
+        for(int i = 1; i < m; ++i){
+            for(int j = 1; j < n; ++j){
+                grid[i][j] += Math.Min(grid[i-1][j], grid[i][j-1]);
+                //Print(grid, 0, j);   
+
+            }
+        }
+        return grid[m-1][n-1];
+    }
+    
+    private void Print(int[][] grid) {
+        for(int i = 0; i < grid.Length; i++) {
+            for(int j = 0; j < grid[0].Length; j++) {
+                Console.WriteLine($"Value at index[{i}][{j}] is {grid[i][j]}");
+            }
+        }
+    }
+    
+    private void Print(int[][]grid, int i , int j){
+        Console.WriteLine($"Value at index[{i}][{j}] is {grid[i][j]}");
+
     }
 }
