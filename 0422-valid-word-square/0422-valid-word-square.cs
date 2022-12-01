46b55a73d03b88697cdea077d2d2eb8bfdb376d3
Jodex Saint-Vil <jsaintvi@aol.com>
2022-11-30 22:50:19 -0500

    Time: 107 ms (100.00%), Space: 41.9 MB (30.77%) - LeetHub

--- 0422-valid-word-square/0422-valid-word-square.cs
+++ 0422-valid-word-square/0422-valid-word-square.cs
@@ -1,18 +1,19 @@
 public class Solution {
     public bool ValidWordSquare(IList<string> words) {
         
-        for(int i = 0; i < words.Count; i++) {
-            for(int j = 0; j < words[i].Length; j++) {
-                int row = j;
-                int col = i;
-                if (row > words.Count-1)
+        int rowCount = words.Count;
+        
+        for(int r = 0; r < rowCount; r++)
+        {
+            for(int c = 0; c < words[r].Length; c++)
+            {
+                if(c >= rowCount || r >= words[c].Length || words[r][c] != words[c][r])
+                {
                     return false;
-                if (words[row].Length -1 < col)
-                    return false;
-                if (words[i][j] != words[j][i])
-                    return false;
+                }
             }
         }
+        
         return true;
     }
 }
