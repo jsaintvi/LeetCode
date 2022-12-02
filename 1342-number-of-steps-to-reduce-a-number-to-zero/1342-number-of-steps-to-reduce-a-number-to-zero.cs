c4e1cc995a45a3417e7cb21638b23a13b5271896
Jodex Saint-Vil <jsaintvi@aol.com>
2022-12-01 21:41:25 -0500

    Time: 24 ms (92.07%), Space: 26.5 MB (14.77%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int NumberOfSteps(int num) {
+        int numSteps = 0;
+        
+        while(num > 0) {
+            if(num % 2 == 0) {
+                num /=2;
+            }else{
+                num -=1;
+            }
+            
+            numSteps++;
+        }
+        
+        return numSteps;
+    }
+}
