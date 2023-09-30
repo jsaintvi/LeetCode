11bf049350b17d8f5b8712f6538c1932938a7a6b
Jodex Saint-Vil <jsaintvi@aol.com>
2023-09-30 00:02:29 -0400

    Time: 94 ms (35.42%), Space: 42.3 MB (47.92%) - LeetHub

--- 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.cs
+++ 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.cs
@@ -1,13 +1,19 @@
 public class Solution {
     public int MaxDistToClosest(int[] seats) {
-        int res = 0, n = seats.Length, last = -1;
+        int res = 0, n = seats.Length, prevPersonIndex  = -1;
+        
         for (int i = 0; i < n; ++i) {
-            if (seats[i] == 1) {
-                res = last < 0 ? i : Math.Max(res, (i - last) / 2);
-                last = i;
+            if (seats[i] == 1) { // // If a person is sitting at position i
+                
+                // Case 1: The first person, so distance is i
+                // Case 2: Calculate the distance to the nearest person on both sides
+                res = prevPersonIndex  == -1 ? i : Math.Max(res, (i - prevPersonIndex ) / 2);
+                prevPersonIndex  = i;
             }
         }
-        res = Math.Max(res, n - last - 1);
+        
+        // Check for the distance to the last seat
+        res = Math.Max(res, n - prevPersonIndex  - 1);
         return res;
     }
 }
