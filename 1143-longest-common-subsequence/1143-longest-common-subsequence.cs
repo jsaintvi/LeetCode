c1a570c1caa2ee567c1b4c02bdec069452038cd7
Jodex Saint-Vil <jsaintvi@aol.com>
2023-10-08 16:56:05 -0400

    Time: 68 ms (74.09%), Space: 36 MB (100.00%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cs
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cs
@@ -2,10 +2,10 @@ public class Solution {
     public int LongestCommonSubsequence(string text1, string text2) {
         int m = text1.Length;
         int n = text2.Length;
-        int[,] lcs = new int[m+1, n+1];
+        int[,] lcs = new int[2, n+1];
         
         // initialize first row and first column to 0 (base case)
-        for(int row = 0; row <= m; row++) {
+        for(int row = 0; row <= 1; row++) {
             lcs[row, 0] = 0;
         }
         
@@ -17,13 +17,13 @@ public class Solution {
             for(int j = 1; j <= n; j++) {
                 if(text1[i-1].Equals(text2[j-1]))
                 {
-                    lcs[i,j] = 1 + lcs[i-1, j-1];
+                    lcs[i%2,j] = 1 + lcs[(i-1) % 2, j-1];
                 } else { // chars not equal
-                    lcs[i,j] = Math.Max(lcs[i-1, j], lcs[i, j-1]);
+                    lcs[i%2,j] = Math.Max(lcs[(i-1) % 2, j], lcs[i % 2, j-1]);
                 }
             }
         }
         
-        return lcs[m, n];
+        return lcs[m%2, n];
     }
 }
