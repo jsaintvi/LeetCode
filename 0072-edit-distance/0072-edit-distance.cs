efe67fbd87163d50cfafa9977f56495b26fdea04
Jodex Saint-Vil <jsaintvi@aol.com>
2023-10-09 15:05:11 -0400

    Time: 73 ms (26.50%), Space: 55.3 MB (15.67%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cs
+++ 0072-edit-distance/0072-edit-distance.cs
@@ -4,49 +4,34 @@ public class Solution {
         int n = word2.Length;
         int[,] dp = new int[m+1,n+1];
         
-        for(int i = 0; i <= m; i++) {
-            for(int j = 0; j <= n; j++) {
-                dp[i,j] = -1;
+        // intialize row 0 and COL 0 to 0
+        for(int row = 0; row <= m; row++) {
+            dp[row, 0] = row;
+        }
+        
+        for(int col = 0; col <= n; col++) {
+            dp[0, col] = col;
+        }
+        
+        for(int row = 1; row <= m; row++) {
+            for(int col = 1; col <= n; col++) {
+                if(word1[row-1] == word2[col-1]) {
+                    dp[row, col] = dp[row - 1, col - 1];
+                } else {
+                    // --------------------------------------------------------------------
+                    //| We need to explore all 3 options: 
+                    //| 1 - INSERT: MinDIstance(word1, word2, m, n-1) 
+                    //| 2 - DELETE: MinDIstance(word1, word2, m - 1, n)  
+                    //| 3 - REPLACE: (MinDistance(word1, word2, m-1, n-1))
+                    //---------------------------------------------------------------------
+                    dp[row, col] = 1 + Min(dp[row, col-1], dp[row-1, col], dp[row-1, col-1]);
+                }
             }
         }
-            
-        return MinDistance(word1, word2, m, n, dp);
+        
+        return dp[m, n];      
     }
-    
-    private int MinDistance(string word1, string word2, int m, int n, int[,] dp) {
-        // TO transform 1 string into empty string, we delete all its chars. Same way, to transform an EMPTY string into non-empty string
-        if(m == 0) { // empty
-            return n;
-        }
-        
-        if( n == 0) {
-            return m;
-        }
-        
-        if(dp[m,n] != -1) {
-            return dp[m,n];
-        }
-                
-        if(word1[m-1] == word2[n-1]) { // chars are equal
-            dp[m,n] =  MinDistance(word1, word2, m-1, n-1, dp);
-        } else {
-            // --------------------------------------------------------------------
-            //| We need to explore all 3 options: 
-            //| 1 - INSERT: MinDIstance(word1, word2, m, n-1) 
-            //| 2 - DELETE: MinDIstance(word1, word2, m - 1, n)  
-            //| 3 - REPLACE: (MinDistance(word1, word2, m-1, n-1))
-            //---------------------------------------------------------------------
-            
-            dp[m,n] =  1 + Min(
-                MinDistance(word1, word2, m, n-1, dp),
-                MinDistance(word1, word2, m - 1, n, dp),
-                MinDistance(word1, word2, m - 1, n-1, dp)
-            );
-        }
-        
-        return dp[m,n];
-    }
-    
+     
     private int Min(params int[] vals) {
         int min = int.MaxValue;
         foreach(var val in vals){
