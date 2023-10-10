1ae592e15e7f03afee9e26ef4c594ecdbecc6d40
Jodex Saint-Vil <jsaintvi@aol.com>
2023-10-10 14:45:33 -0400

    Time: 246 ms (36.94%), Space: 152.4 MB (62.31%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    private readonly IList<IList<int>> output = new List<IList<int>>();
+    public IList<IList<int>> Combine(int n, int k) {
+        List<int> temp = new();
+        KCombine(temp, 0, n, k);
+        
+        return output;
+    }
+    
+    private void KCombine(List<int> temp, int start, int end, int k) {
+        var index = temp.Count;
+        if(index == k) {
+            output.Add(new List<int>(temp));
+            return;
+        }
+        
+        for(int i = start; i < end && (end-i + 1) >= (k - index); i++) {
+            temp.Add(i + 1);
+            KCombine(temp, i + 1, end, k);
+            
+            // remove last elem for backtracking
+            temp.RemoveAt(temp.Count - 1);
+        }
+    }
+}
