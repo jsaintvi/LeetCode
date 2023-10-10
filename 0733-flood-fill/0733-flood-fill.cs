1dee20a3dcb47822d5d0fea52bd32e3281d44c2e
Jodex Saint-Vil <jsaintvi@aol.com>
2023-10-10 13:46:27 -0400

    Time: 134 ms (72.73%), Space: 45.8 MB (90.91%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cs
+++ 0733-flood-fill/0733-flood-fill.cs
@@ -1,27 +1,46 @@
 public class Solution {
+    private class Point {
+        public int X {get;set;}
+        public int Y {get; set;}
+        
+        public Point(int x, int y) {
+            this.X = x;
+            this.Y = y;
+        }
+    }
     public int[][] FloodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc] != color) {
-            FloodFillHelper(image, sr, sc, image[sr][sc], color);
+        
+        if(image[sr][sc] == color)
+        {
+            return image;
         }
         
-        return image;
-    }
-    
-    private void FloodFillHelper(int[][] image, int row, int col, int originalColor, int newColor) {
+        int originalColor = image[sr][sc];
         int rowCount = image.Length;
         int colCount = image[0].Length;
         
-        if(row < 0 || row >= rowCount || col < 0 || col >= colCount)
-            return;
+        Stack<Point> st = new();
+        st.Push(new Point(sr, sc));
         
-        if(image[row][col] == originalColor) {
-            image[row][col] = newColor;
-                    
-            FloodFillHelper(image, row - 1, col, originalColor, newColor);// up
-            FloodFillHelper(image, row + 1, col, originalColor, newColor);// down
-            FloodFillHelper(image, row , col - 1, originalColor, newColor);// left
-            FloodFillHelper(image, row , col + 1, originalColor, newColor);// right
+        while(st.Count > 0) {
+            
+            int row = st.Peek().X;
+            int col = st.Peek().Y;
+            st.Pop();
+            
+            // check for invalid i.e out of bounds cells
+            if(row < 0 || row >= rowCount || col < 0 || col >= colCount) continue;
+            if(image[row][col] == originalColor) {
+                // paint with new color and push adjacents to stack
+                image[row][col] = color;
+                
+                st.Push(new Point(row + 1, col));
+                st.Push(new Point(row - 1, col));
+                st.Push(new Point(row, col - 1));
+                st.Push(new Point(row, col + 1));
+            }
         }
-
+        
+        return image;
     }
 }
