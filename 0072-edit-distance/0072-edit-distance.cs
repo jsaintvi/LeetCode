5609a39f5be5ab270fb299eaabb8c3dfa4302110
Jodex Saint-Vil <jsaintvi@aol.com>
2023-10-31 13:26:04 -0400

    Time: 69 ms (33.22%), Space: 54.8 MB (10.85%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cs
+++ 0072-edit-distance/0072-edit-distance.cs
@@ -5,40 +5,31 @@ public class Solution {
         
         int[,] dp = new int[m+1, n+1];
         
-        for(int i = 0; i < m+1; i++) {
-            for(int j = 0; j < n+1; j++) {
-                dp[i,j] = -1;
+        for(int row = 0; row < m+1; row++)
+            dp[row,0] = row;
+        
+        for(int col = 0; col < n+1; col++)
+            dp[0, col] = col;
+        
+        for(int row = 1; row < m+1; row++) {
+            for(int col = 1; col < n+1; col++) {
+                if(word1[row-1] == word2[col-1]) {
+                    dp[row,col] = dp[row-1, col-1];
+                } else {
+                    dp[row,col] = 1 + Min(
+                        // INSERT
+                        dp[row, col-1], 
+                        // Delete
+                        dp[row-1, col],
+                        // Replace
+                        dp[row-1,col-1]
+                    );
+                }
             }
-                
         }
-         return   MinDistance(word1, word2, m,n, dp) ;
+         return  dp[m,n];
     }
     
-    private int MinDistance(string word1, string word2, int m, int n, int[,] dp) {
-        if(m == 0)
-            return n;
-        if(n== 0)
-            return m;
-        
-        if(dp[m,n] != -1)
-            return dp[m,n];
-        
-        if(word1[m-1] == word2[n-1]) {
-            dp[m,n] = MinDistance(word1, word2, m-1, n-1, dp);
-        } else{
-            dp[m,n] = 1+ Min(
-            //Insert
-               MinDistance(word1, word2, m, n-1, dp),
-            // Delete
-                MinDistance(word1, word2, m-1, n, dp),
-            // Replace
-                MinDistance(word1, word2, m-1, n-1, dp)
-            );
-        }
-           
-        return dp[m,n];
-    }
-     
     private int Min(params int[] vals) {
         int min = int.MaxValue;
         foreach(var val in vals){
