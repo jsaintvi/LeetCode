d71256abedee3518db0e18d0ead3db29ca494202
Jodex Saint-Vil <jsaintvi@aol.com>
2023-11-10 22:27:45 -0500

    Time: 137 ms (29.76%), Space: 43.5 MB (46.10%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cs
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cs
@@ -1,20 +1,16 @@
 public class Solution {
     public int[] Intersection(int[] nums1, int[] nums2) {
-        HashSet<int>seen = new(nums1);
-        HashSet<int> nums2Set =  new (nums2);
+        var setOne = new HashSet<int>(nums1);
+        var setTwo = new HashSet<int>(nums2);
         
-        foreach(var x in nums1)
+        var result = new List<int>();
+
+        foreach (var item in setOne)
         {
-            seen.Add(x);
+            if(setTwo.Contains(item))
+                result.Add(item);
         }
-        
-        List<int> ans = new();
-        foreach(var x in nums2Set){
-            if(!seen.Contains(x)) continue;
-            
-            ans.Add(x);
-        }
-        
-        return ans.ToArray();
+
+        return result.ToArray();
     }
 }
