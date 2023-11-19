f54e010ed76bea0fba433062bf58ade69a75c8bb
Jodex Saint-Vil <jsaintvi@aol.com>
2023-11-19 13:53:13 -0500

    Time: 88 ms (64.00%), Space: 39.9 MB (88.00%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cs
@@ -0,0 +1,6 @@
+public class Solution {
+    public bool StoneGame(int[] piles) {
+        // SEE: https://leetcode.com/problems/stone-game/discuss/154610/DP-or-Just-return-true
+        return true;
+    }
+}
