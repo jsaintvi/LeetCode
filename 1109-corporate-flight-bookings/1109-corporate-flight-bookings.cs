d3860abc2cbd3cccf23b6bc1e737ad985af1a55a
Jodex Saint-Vil <jsaintvi@aol.com>
2023-11-26 18:48:45 -0500

    Time: 308 ms (51.85%), Space: 63.8 MB (70.37%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cs
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cs
@@ -1,26 +1,23 @@
 public class Solution {
     public int[] CorpFlightBookings(int[][] bookings, int n) {
-        int[] flightCapacity = new int[n+1];
+        var answer = new int[n];
         
-        foreach(var booking in bookings)
-        {
-            var first = booking[0];
-            var last = booking[1];
-            var total = booking[2];
+        foreach (int[] booking in bookings) {
+            int first = booking[0];
+            int last = booking[1];
+            int seats = booking[2];
             
-            for(int i = first; i <= last; i++)
-                flightCapacity[i]+= total;
+            answer[first - 1] += seats; // minus cause flights are 1-based index
+            if (last < n) {
+                answer[last] -= seats;
+            }
         }
         
-        int[] ans = new int[n];
-        int pos = 0;
-
-        for(int i = 1; i < flightCapacity.Length; i++)
-        {
-            ans[i-1] = flightCapacity[i];
+        for (int i = 1; i < n; i++) {
+            answer[i] += answer[i - 1];
         }
         
-        return ans;
+        return answer;
         
     }
 }
