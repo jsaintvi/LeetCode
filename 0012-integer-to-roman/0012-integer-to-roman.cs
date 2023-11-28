dd730e18aca140d31f202a0056d52faafc1ec215
Jodex Saint-Vil <jsaintvi@aol.com>
2023-11-28 16:03:55 -0500

    Time: 75 ms (41.88%), Space: 46.6 MB (18.19%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cs
+++ 0012-integer-to-roman/0012-integer-to-roman.cs
@@ -1,15 +1,29 @@
 public class Solution {
     public string IntToRoman(int num) {
-        string[] romanDigits = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-        int[] decimalValues = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
         StringBuilder romanNumber = new StringBuilder();
 
-        for (int i = 0; i < decimalValues.Length; i++)
+        Dictionary<int, string> intToRomanMap = new(){
+            {1000, "M"},
+            {900, "CM"},
+            {500, "D"},
+            {400, "CD"},
+            {100, "C"},
+            {90, "XC"},
+            {50, "L"},
+            {40, "XL"},
+            {10, "X"},
+            {9, "IX"},
+            {5, "V"},
+            {4, "IV"},
+            {1, "I"}
+        };
+        
+        foreach ( var @key in intToRomanMap.Keys)
         {
-            while (num >= decimalValues[i])
+            while (num >= @key)
             {
-                romanNumber.Append(romanDigits[i]);
-                num -= decimalValues[i];
+                romanNumber.Append(intToRomanMap[@key]);
+                num -= @key;
             }
         }
 
