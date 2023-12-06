f194b59a0a97134beb949fd7c3048a81ab6ae77b
Jodex Saint-Vil <jsaintvi@aol.com>
2023-12-05 23:33:51 -0500

    Time: 323 ms (11.11%), Space: 49.6 MB (22.22%) - LeetHub

--- 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.cs
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.cs
@@ -2,22 +2,19 @@ public class Solution
 {
     public int[] CountPoints(int[][] points, int[][] queries)
     {
-        List<(double x, double y, int idx)> circlePoints = new List<(double x, double y, int idx)>();
-        for (int i = 0; i < points.Length; i++)
-        {
-            circlePoints.Add((points[i][0], points[i][1], i));
-        }
-
         List<(double x, double y, int count)> circles = new List<(double x, double y, int count)>();
         for (int i = 0; i < queries.Length; i++)
         {
-            double x = queries[i][0];
-            double y = queries[i][1];
-            double r = queries[i][2];
+            int x = queries[i][0];
+            int y = queries[i][1];
+            int r = queries[i][2];
+            
             int count = 0;
-            foreach (var (xp, yp, idx) in circlePoints)
+            foreach (var point in points)
             {
-                // calc distance 
+                var xp = point[0];
+                var yp = point[1];
+                // calc distance  between the 2 points
                 double d = Math.Sqrt(Math.Pow(xp - x, 2) + Math.Pow(yp - y, 2));
                 if (d <= r)
                 {
