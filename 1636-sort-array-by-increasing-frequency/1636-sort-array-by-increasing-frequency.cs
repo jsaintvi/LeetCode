6f85f01c10cb742c92e6660a9cc5d2fb1a183830
Jodex Saint-Vil <jsaintvi@aol.com>
2023-12-17 12:49:34 -0500

    Time: 132 ms (80.39%), Space: 44.7 MB (70.59%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cs
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cs
@@ -1,13 +1,57 @@
 public class Solution {
+    private class NumInfo{
+        public int Num{get;set;}
+        public int Priority {get;set;}
+    }
     public int[] FrequencySort(int[] nums) {
-        Dictionary<int,int> hash = new();
-        foreach(var num in nums){
-            if(!hash.ContainsKey(num)) hash.Add(num, 0);
-            
-            hash[num] += 1;
+        var dict = new Dictionary<int, int>();
+        
+        foreach(var num in nums)
+        {
+            if(dict.ContainsKey(num))
+            {
+                dict[num]++;
+            }
+            else
+            {
+                dict[num] = 1;
+            }
         }
         
-        var sortedArray = nums.OrderBy(x => hash[x]).ThenBy(x => -x).ToArray();
+        var priorityQueue = new PriorityQueue<int, NumInfo>(new NumComparer());
+        
+        foreach(var kvp in dict)
+        {
+            priorityQueue.Enqueue(kvp.Key, new NumInfo(){Num = kvp.Key, Priority = kvp.Value});
+        }
+        
+        var sortedArray = new int[nums.Length];
+        int pos = 0;
+        
+        while(priorityQueue.Count > 0)
+        {
+            
+            priorityQueue.TryDequeue(out int num, out NumInfo frequency);
+            
+            for(int i = 0; i < frequency.Priority; i++)
+            {
+                sortedArray[pos++] = num;
+            }
+        }
+        
         return sortedArray;
+    }
+    
+    private class NumComparer : IComparer<NumInfo>
+    {
+        public int Compare(NumInfo x, NumInfo y) {
+            if(x.Priority == y.Priority) {
+                return y.Num.CompareTo(x.Num);
+            } else{
+                return x.Priority.CompareTo(y.Priority);
+            }
+            
+            
+        }
     }
 }
