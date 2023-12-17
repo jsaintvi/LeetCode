18365537464dfd82965d4215be95b4ea9753a2fd
Jodex Saint-Vil <jsaintvi@aol.com>
2023-12-17 12:50:59 -0500

    Time: 148 ms (11.76%), Space: 44.3 MB (96.08%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cs
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cs
@@ -1,8 +1,4 @@
 public class Solution {
-    private class NumInfo{
-        public int Num{get;set;}
-        public int Priority {get;set;}
-    }
     public int[] FrequencySort(int[] nums) {
         var dict = new Dictionary<int, int>();
         
@@ -18,40 +14,12 @@ public class Solution {
             }
         }
         
-        var priorityQueue = new PriorityQueue<int, NumInfo>(new NumComparer());
-        
-        foreach(var kvp in dict)
-        {
-            priorityQueue.Enqueue(kvp.Key, new NumInfo(){Num = kvp.Key, Priority = kvp.Value});
-        }
-        
-        var sortedArray = new int[nums.Length];
-        int pos = 0;
-        
-        while(priorityQueue.Count > 0)
-        {
-            
-            priorityQueue.TryDequeue(out int num, out NumInfo frequency);
-            
-            for(int i = 0; i < frequency.Priority; i++)
-            {
-                sortedArray[pos++] = num;
-            }
-        }
-        
-        return sortedArray;
-    }
-    
-    private class NumComparer : IComparer<NumInfo>
-    {
-        public int Compare(NumInfo x, NumInfo y) {
-            if(x.Priority == y.Priority) {
-                return y.Num.CompareTo(x.Num);
-            } else{
-                return x.Priority.CompareTo(y.Priority);
-            }
-            
-            
-        }
+        Array.Sort(nums, (x,y)=> {
+            if (dict[x] == dict[y])
+                return y-x;
+            return dict[x] - dict[y];
+        });
+
+        return nums;
     }
 }
