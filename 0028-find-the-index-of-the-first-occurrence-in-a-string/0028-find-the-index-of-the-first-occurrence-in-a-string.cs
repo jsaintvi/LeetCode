6f3d091c77bce0dcde7207c0edb5e73699ad30ad
Jodex Saint-Vil <jsaintvi@aol.com>
2024-01-02 13:23:16 -0500

    Time: 92 ms (5.47%), Space: 39 MB (6.11%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cs
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cs
@@ -1,12 +1,5 @@
 public class Solution {
     public int StrStr(string haystack, string needle) {
-        if(string.IsNullOrEmpty(haystack) && string.IsNullOrEmpty(needle))
-            return 0;
-        
-        var index = haystack.IndexOf(needle);
-        if(string.IsNullOrEmpty(haystack) || index == -1)
-            return -1;
-        
-        return index;
+        return  haystack.IndexOf(needle);
     }
 }
