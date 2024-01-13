fc8a79d2c182c63c3efaca7e428ef889e27c2664
Jodex Saint-Vil <jsaintvi@aol.com>
2024-01-13 17:12:39 -0500

    Time: 245 ms (50.00%), Space: 70.7 MB (29.41%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.cs
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.cs
@@ -2,32 +2,20 @@ public class Solution {
     public int CountDistinctIntegers(int[] nums) {
         
         int n = nums.Length;
-        List<int> lst = new List<int>();
         
         
+        HashSet<int> numSet = new();
         foreach(var num in nums) {
             
             var reversed = ReverseDigit(num);
             
+            
             //Console.WriteLine($"Num: {num} - Reversed {reversed}");
-            lst.Add(num);
-            lst.Add(reversed);
+            numSet.Add(num);
+            numSet.Add(reversed);
         }
         
-        HashSet<int> numSet = new();
-        
-        int distinctCount = 0;
-        foreach(var x in lst){
-            if(numSet.Contains(x)) continue;
-            
-            
-            distinctCount+=1;
-            
-            numSet.Add(x);
-        }
-        
-        
-        return distinctCount;
+        return numSet.Count;
     }
     
 
