cbfbe4c6435ea29041b64afa954c03b642e1fa33
Jodex Saint-Vil <jsaintvi@aol.com>
2024-01-22 22:07:11 -0500

    Time: 94 ms (87.14%), Space: 49.6 MB (11.88%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cs
+++ 0022-generate-parentheses/0022-generate-parentheses.cs
@@ -1,29 +1,31 @@
 public class Solution {
+    public record State(string combination, int openCount, int closeCount);
+    
     public IList<string> GenerateParenthesis(int n) {
         List<string> result = new List<string>();
-        GenerateParenthesisHelper(result, "", 0, 0, n);
+        
+        Queue<State> q = new();
+        q.Enqueue(new State("(", 1, 0));
+        
+        while(q.Count > 0) {
+            var curr = q.Dequeue();
+            if(curr.openCount == n && curr.closeCount == n)
+            {
+                result.Add(curr.combination);
+                continue;
+            }
+            
+            if(curr.openCount < n) {
+                q.Enqueue(new State(curr.combination + "(", curr.openCount + 1, curr.closeCount));
+            }
+            
+            if(curr.closeCount < n &&curr.closeCount < curr.openCount) {
+                q.Enqueue(new State(curr.combination + ")", curr.openCount, curr.closeCount + 1));
+ 
+            }
+        }
+            
         return result;
         
-    }
-    
-    private void GenerateParenthesisHelper(List<string> result, string current, int open, int close, int max)
-    {
-        // Console.WriteLine($"---------{current}");
-
-        if (current.Length == max * 2)
-        {
-            result.Add(current);
-            return;
-        }
-
-        if (open < max)
-        {
-            GenerateParenthesisHelper(result, current + "(", open + 1, close, max);
-        }
-
-        if (close < open)
-        {
-            GenerateParenthesisHelper(result, current + ")", open, close + 1, max);
-        }
     }
 }
