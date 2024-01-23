ff165345efec6b754a4d39ea93c32c356ffae375
Jodex Saint-Vil <jsaintvi@aol.com>
2024-01-22 21:53:48 -0500

    Time: 105 ms (66.25%), Space: 49.4 MB (13.48%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public IList<string> GenerateParenthesis(int n) {
+        List<string> result = new List<string>();
+        GenerateParenthesisHelper(result, "", 0, 0, n);
+        return result;
+        
+    }
+    
+    private void GenerateParenthesisHelper(List<string> result, string current, int open, int close, int max)
+    {
+        // Console.WriteLine($"---------{current}");
+
+        if (current.Length == max * 2)
+        {
+            result.Add(current);
+            return;
+        }
+
+        if (open < max)
+        {
+            GenerateParenthesisHelper(result, current + "(", open + 1, close, max);
+        }
+
+        if (close < open)
+        {
+            GenerateParenthesisHelper(result, current + ")", open, close + 1, max);
+        }
+    }
+}
