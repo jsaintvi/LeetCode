9b4c837add137441b297e91ce6cd27c36e9aa758
Jodex Saint-Vil <jsaintvi@aol.com>
2024-04-01 22:51:35 -0400

    Time: 69 ms (76.75%), Space: 38.8 MB (22.11%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cs
+++ 0118-pascals-triangle/0118-pascals-triangle.cs
@@ -1,29 +1,22 @@
 public class Solution {
     public IList<IList<int>> Generate(int numRows) {
-        IList<IList<int>> triangle = new List<IList<int>>();
+       IList<IList<int>> triangle = new List<IList<int>>();
         
         if (numRows == 0)
             return triangle;
         
-        // Initialize the first row with 1
-        IList<int> firstRow = new List<int>();
-        firstRow.Add(1);
-        triangle.Add(firstRow);
-        
-        for (int i = 1; i < numRows; i++) {
-            IList<int> prevRow = triangle[i - 1];
-            IList<int> currRow = new List<int>();
-            
-            // The first element of each row is always 1
-            currRow.Add(1);
+        for (int i = 0; i < numRows; i++) {
+            IList<int> currRow = new List<int>(i + 1);
+            currRow.Add(1); // First element of each row is always 1
             
             // Calculate the elements in the current row based on the previous row
             for (int j = 1; j < i; j++) {
+                IList<int> prevRow = triangle[i - 1];
                 currRow.Add(prevRow[j - 1] + prevRow[j]);
             }
             
-            // The last element of each row is always 1
-            currRow.Add(1);
+            if (i > 0) // Skip if it's the first row
+                currRow.Add(1); // Last element of each row is always 1
             
             triangle.Add(currRow);
         }
